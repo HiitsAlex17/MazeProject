bfac4669c0bb9dc7b32c8e3332846dd6c38d61aa
HiitsAlex17 <59703970+HiitsAlex17@users.noreply.github.com>
2020-01-09 11:59:27 -0600

    Initial commit

---
+++ Assets/Scripts/BallControls.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControls : MonoBehaviour
+{
+    public float moveStrength;
+    public float jumpStrength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * jumpStrength);
+        }
+        if (Input.GetKey("w"))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.forward * moveStrength);
+        }
+        if (Input.GetKey("a"))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.left * moveStrength);
+        }
+        if (Input.GetKey("s"))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.back * moveStrength);
+        }
+        if (Input.GetKey("d"))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.right * moveStrength);
+        }
+
+    }
+}
